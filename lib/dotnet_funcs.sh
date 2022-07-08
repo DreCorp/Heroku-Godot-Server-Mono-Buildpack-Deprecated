a2ebf032bbe3ccba7d6a7f81a523eede3c875e41
DreCorp <adanjuk@gmail.com>
2022-07-09 00:55:20 +0100

    minor fixes

--- lib/dotnet_funcs.sh
+++ lib/dotnet_funcs.sh
@@ -12,14 +12,16 @@ function download_dotnet_script() {
         wget $DOTNET_SCRIPT_URL
 
         #cp DOTNET_SCRIPT_FILE $CACHE_DIR
-        cp DOTNET_SCRIPT_FILE $BUILD_DIR
+        cp dotnet-install.sh $BUILD_DIR
 
         # give file permission
-        chmod +x $BUILD_DIR/.DOTNET_SCRIPT_FILE
+        chmod +x $BUILD_DIR/.dotnet-install.sh
 
         output_section "Installing DotNet..."
         #./dotnet-install $CACHE_DIR/dotnet
-        ./dotnet-install.sh --install-dir $BUILD_DIR/dotnet
+        
+        #
+        $BUILD_DIR/dotnet-install.sh --install-dir $BUILD_DIR/dotnet
 
         # add to path
         export PATH=$PATH:~/$BUILD_DIR/dotnet
